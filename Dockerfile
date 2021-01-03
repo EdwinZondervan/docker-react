0e87dfa961f711579f7f4df7397baa8f5302cb7c
RedArmyNL <youri.zondervan@gmail.com>
2021-01-03 16:52:20 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+#build phase 
+FROM node:alpine as builder 	
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build			# build folder -> /app/build, deze willen we kopieren
+ 
+#run phase, begin met een nieuwe image 
+FROM nginx					
+#COPY --from=builder /app/build /usr/share/nginx/html				#copy iets from different phase. 
+COPY --from=builder /app/build /usr/share/nginx/html				
+#We hoeven nginx niet expliciet te starten omdat dit het default commando is 
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,23 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
